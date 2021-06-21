d7de8a1be078c48ab31c546658b9c58bf7679dbd
timtimtimu <mahfuzarahmantimu@gmail.com>
2021-06-21 20:33:35 +0600

    banner_slider

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,15 @@ $(document).ready(function () {
         navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
     });
 });
+
+$(document).ready(function () {
+    $(".banner_bottom_content_slide").owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots: false,
+        items: 3,
+        autoplay: false,
+       
+    });
+});
